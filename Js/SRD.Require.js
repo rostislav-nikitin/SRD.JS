e87b0bd61114b87ce9a33955a659f24d2f3fc973
Rostislav Nikitin <rostislav.nikitin@gmail.com>
2016-06-20 20:41:36 +0300

    01. Added async support.     Use cases:       .useConfig({isSplitName: true, basePath: 'SRD.Require.Examples'})         .require('SRD.Namespace', null, {isSplitName: false, basePath: '../Js'})         .require('SRD.Type', null, {isSplitName: false, basePath: '../Js'})         .require("TestNamespace.InnerTestNamespace.ClassOne")         .onSuccess(function() { ns('SRD.Collection').Queue = (function(){ writeLine('SRD.Collection.Queue type contructed...'); return Array; })(); });

--- Js/SRD.Require.js
+++ Js/SRD.Require.js
@@ -14,11 +14,13 @@
 #                                                                                                                      #
 +======================================================================================================================+
 */
-//
+//TODO: Implement require as deferred. To make possible to make: require('class').require('class2').execute(function(){}, context);
+//	First it will load asynchronously class1, second class two, and at least when all dependencies are 
+//	loaded it will execute class3 code
 (function(context, isExportToContext)
 {
 	// Private static constants
-	var 	TypesNames = { Undefined: "undefined", String: "string", Object: "object" },
+	var 	TypesNames = { Undefined: 'undefined', String: 'string', Function: 'function', Object: 'object' },
 		Zero = 0,
 		One = 1,
 		EmptyString = '',
@@ -27,7 +29,7 @@
 		DefaultIsSplitName = false,
 		DefaultIsExportToContext = true,
 		DefaultIsAddJsExtension = true,
-		DefaultJsExtension = "js",
+		DefaultJsExtension = 'js',
 		DefaultConfig = { isSplitName: DefaultIsSplitName, basePath: '.', isAddJsExtension:  true, jsExtension: 'js' },
 	// Private static variables
 		_context = context || this,
@@ -71,18 +73,112 @@
 
 		this.require = function(nameOrCollection, pathOrConfig, config)
 		{
+			var deferred = new Deferred();
 			//alert(pathOrConfig);
 			if(typeof(nameOrCollection) === TypesNames.Object && nameOrCollection !== null && "length" in nameOrCollection)
 			{
-				requireCollection(nameOrCollection, pathOrConfig);
+				requireCollection(nameOrCollection, pathOrConfig, deferred);
 			}
 			else if(typeof(nameOrCollection) === TypesNames.String)
 			{
-				requireOne(nameOrCollection, pathOrConfig, config);
+				requireOne(nameOrCollection, pathOrConfig, config, deferred);
 			}
 
-			return _this;
+			return deferred;
 		}
+
+
+		// The deferred class
+		var Deferred = (function(require)
+			{
+				var 	Zero = 0,
+					One = 1;
+
+				function copyArguments(inArgs)
+				{
+					var result = [];
+
+					for(var index = Zero; index < inArgs.length; index++)
+					{
+						result.push(inArgs[index]);
+					}
+
+					return result;
+				}
+
+				function constructor()
+				{
+					var 	_this = this,
+						_arguments,
+						_tasksCount = Zero,
+						_success,
+						_error;
+
+					this.require = function()
+					{
+						_this.deferred = new constructor();
+						_this.deferred.begin();
+
+						_arguments = copyArguments(arguments);
+
+						_this.onSuccess(require);
+						_this.onError(_this.deferred.error);
+
+        					return _this.deferred;
+					}
+
+					this.begin = function()
+					{
+						_tasksCount++;
+					}
+
+					this.error = function(message)
+					{
+						_tasksCount--;
+
+						if(_error instanceof Function)
+						{
+							_error.apply(this, [message]);
+						}
+					}
+
+					this.success = function()
+					{
+
+						_tasksCount--;
+						if(_tasksCount === Zero && _success instanceof Function)
+						{
+							//debugger;
+							var applyResult = _success.apply(this, _arguments);
+							if(typeof applyResult !== TypesNames.Undefined 
+								&& applyResult !== null
+								&& "onSuccess" in applyResult
+								&& "onError" in applyResult)
+							{
+								applyResult
+									.onSuccess(_this.deferred.success)
+									.onError(_this.deferred.error);
+							}
+						}
+					}
+
+					this.onSuccess = function(callback)
+					{
+						_success = callback;
+						//alert(_success);
+						return _this;
+					}
+					this.onError = function(callback)
+					{
+						_error = callback;
+						return _this;
+					}
+
+				}
+
+				return constructor;
+			})(this.require);
+
 
 		// Use config members
 		function useConfigSafe(config)
@@ -104,27 +200,49 @@
 
 	        // Require members
 		// Require collection members
-		function requireCollection(collection, config)
+		function requireCollection(collection, config, deferred)
 		{
 			for(var index = Zero; index < collection.length; index++)
 			{
-				requireOne(collection[index].name, collection[index].path, config);
+				requireOne(collection[index].name, collection[index].path, config, deferred);
 			}
 		}
 
 
-		// Require one members
-		function requireOne(name, path, config)
+		function copy(fromObj, toObj)
 		{
-			config = config || _config;
+			for(var propertyName in fromObj)
+			{
+				var propertyValue = fromObj[propertyName];
+				if(!(propertyValue instanceof Function))
+				{
+					toObj[propertyName] = propertyValue;
+				}
+			}
+		}
+
+		function combine(objOne, objTwo)
+		{
+			var result = {};
+			copy(objOne, result);
+			copy(objTwo, result);
+
+			return result;
+		}
+
+
+		// Require one members
+		function requireOne(name, path, config, deferred)
+		{
+			config = combine(_config, config);
 
 			if(config.isSplitName)
 			{
-				load(name.split(PointCharacter).join(SlashCharacter), path, config);
+				load(name.split(PointCharacter).join(SlashCharacter), path, config, deferred);
 			}
 			else
 			{
-				load(name, path, config);
+				load(name, path, config, deferred);
 			}
 		}
 
@@ -138,13 +256,13 @@
 
 		// Private instance methods
 
-		function load(name, path, config)
+		function load(name, path, config, deferred)
 		{
 			var loadStatus = getLoadStatus(name);
 
 			if(loadStatus !== LoadTypes.Loaded)
 			{
-				addToQueue(name, path, config)
+				addToQueue(name, path, config, deferred)
 			}
 
 			if(loadStatus !== LoadTypes.Loading && loadStatus !== LoadTypes.Loaded)
@@ -153,42 +271,151 @@
 			}
 		}
 
-		function addToQueue(name, path, config)
+		function addToQueue(name, path, config, deferred)
 		{
 			var path = getPath(name, path, config);
 
+			//debugger;
 			if(name in _scripts)
 			{
-				_scripts[name].queue.push(path);
+				scriptEnqueue(name, {path: path, deferred: deferred});
 			}
 			else
 			{
-				_scripts[name] = {loadStatus: LoadTypes.NonLoaded, queue: [path]};
+				_scripts[name] = {loadStatus: LoadTypes.NotLoaded, queue: [{path: path, deferred: deferred}]};
 			}
 		}
 
 		function loadNext(name)
 		{
-			if(name in _scripts && _scripts[name].queue.length != Zero)
+			if(name in _scripts)
 			{
-				var nextPath = _scripts[name].queue.splice(Zero, One);
-				add(name, nextPath);
+				loadNextSafe(name);
+			}
+		}
+
+		function loadNextSafe(name)
+		{
+			if(_scripts[name].queue.length !== Zero)
+			{
+				deferredBegin(name);
+				var next = scriptPeek(name);
+				add(name, next.path);
 			}
 		}
 
 		function loaded(name)
 		{
+			deferredSuccess(name);
 			setLoadStatus(name, LoadTypes.Loaded);
+		}
+
+		function deferredBegin(name)
+		{
+			if(name in _scripts)
+			{
+				scriptPeek(name).deferred.begin();
+			}
+		}
+		
+		function deferredSuccess(name)
+		{
+			if(name in _scripts)
+			{
+				scriptPeek(name).deferred.success();
+			}
+			scriptDequeue(name);
+		}
+
+		function deferredLoadError(name)
+		{
+			//debugger;
+			var errorMessage = 'Can not load script file for the "' + name + '" with path: "' + scriptPeek(name).path + '".';
+			deferredError(name, errorMessage);
+		}
+
+		function deferredError(name, errorMessage)
+		{
+			if(name in _scripts)
+			{
+        			scriptPeek(name).deferred.error(errorMessage);
+				scriptDequeue(name);
+			}
+		}
+
+		var DefaultScriptQueueValue = null;
+
+		function scriptEnqueue(name, value)
+		{
+			if(name in _scripts)
+			{
+				_scripts[name].queue.push(value);
+			}
+		}
+
+		function scriptPeek(name)
+		{
+			var result = DefaultScriptQueueValue;
+
+			if(name in _scripts)
+			{
+				result = scriptPeekSafe(name);
+				result = _scripts[name].queue[Zero];
+			}
+
+			return result;
+		}
+
+		function scriptPeekSafe(name)
+		{
+			var result = DefaultScriptQueueValue, 
+				queue = _scripts[name].queue;
+
+			if(queue.length > Zero)
+			{
+				result = queue[Zero];
+			}
+
+			return result;
+		}
+
+		function scriptDequeue(name)
+		{
+			var result = null;
+
+			if(name in _scripts)
+			{
+				result = _scripts[name].queue.splice(Zero, One);
+			}
+
+			return result;
 		}
 
 		function notLoaded(name)
 		{
-			loadNext(name);
+			if(name in _scripts)
+			{
+				notLoadedSafe(name);
+			}
+		}
+
+		function notLoadedSafe(name)
+		{
+			if(_scripts[name].queue.length === One)
+			{
+				deferredLoadError(name);
+				scriptDequeue();
+			}
+			else
+			{
+				scriptDequeue(name);
+				loadNext(name);
+        		}
 		}
 
 		function setLoadStatus(name, status)
 		{
-			_scripts[name].LoadStatus = status;
+			_scripts[name].loadStatus = status;
 		}
 
 		function getLoadStatus(name)
@@ -246,6 +473,7 @@
 		{
 			var head = getHead(),
 				scriptTag = buildScriptTag(name, path);
+
 			head.appendChild(scriptTag);
 		}
 
