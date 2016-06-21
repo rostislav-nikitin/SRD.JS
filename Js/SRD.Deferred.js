ecdbf25e429cbd180f4591bed6c03fee86ae34c7
Rostislav Nikitin <rostislav.nikitin@gmail.com>
2016-06-21 22:34:30 +0300

    01. SRD.Deferred.js: Initial commit

---
+++ Js/SRD.Deferred.js
@@ -0,0 +1,463 @@
+(function(mountPoint, dependencies)
+{
+		var 	TypesNames = { Undefined: 'undefined', Boolean: 'boolean' },
+			CompleteTypes = { Done: 0, Failed: 1 };
+			Zero = 0,
+			One = 1,
+			Two = 2;
+
+	var _dependencies = dependencies || {},
+		logger = dependencies.logger || null;
+		
+	function constructor()
+	{
+		var 	_this = this,
+			_callParameters = null,
+			// Completed queues. When no success, error handlers attached it collect done, error calls to don't forget them.
+			_completed = [],
+			// The flag that indicates that done or faile was processed (calles succes or error handled)
+			//_isHandled,
+			// The flag that indicates that done or fail was processed (called one of the operations)
+			_isChainProcessed,
+			_deferred = null,
+			_success,
+			_error,
+			_successCommon,
+			_errorCommon,
+			_doneCount = Zero;
+
+
+		// The mehtod that executes a single func with a specified funcArgs
+		// Returns a Deffered object
+		this.single = function(context, method)
+		{
+			enqueue(arguments);
+
+			createDeferred();
+
+			complete();
+
+			return _deferred;
+		}
+
+		// Should be executed before wait for done or error. May be done or fail was called before.
+		function complete()
+		{
+			while(_completed.length !== Zero)
+			{
+				completeOne(_completed[Zero]);
+			}
+		}
+
+		// Complete processors hash. Each processor process it's own complete type: done, failed, etc...
+		var completeProcessors = 
+		{
+			CompleteTypes.Done: function(completeParameters) { continueExecution(completeParameters.callResult); },
+			CompleteTypes.Failed: function(completeParameters) { continueExecution(completeParamters.error); }
+		};
+
+		function completeOne(completeParameters)
+		{
+			completeProcessors[completeParameters.completeType](completeParameters);			
+		}
+
+		// Should be caller when parent operation is done.
+		this.done = function(callResult)
+		{
+			if(isProcess())
+			{
+				done(callResult);
+			}
+		}
+
+		function isProcess()
+		{
+			var result = !_callParameters || !(_callParameters.isDone === true);
+			return result;
+		}
+
+		function done(callResult)
+		{
+			_completed.push({ completeType: CompleteTypes.Done, callResult: callResult });
+
+			_isChainProcessed = false;
+
+			doneInternal(callResult);
+
+			if(_isChainProcessed)
+			{
+				completedDequeue();
+			}
+		}
+
+		function doneInternal(callResult)
+		{
+			// If any returns false - stop chain execution otherwise continue.
+			var isStop = (callSuccess(callResult) === false)
+				| ( _this.successCommon(callResult) === false);
+
+			if(!isStop)
+			{
+				continueExecution(callResult);
+			}
+			{
+				_isChainProcessed = true;
+			}
+		}
+
+		// Should be called when the parent operation is failed.
+		this.failed = function(error)
+		{
+			if(isProcess())
+			{
+				failed(error);
+			}
+		}
+
+		function failed(error)
+		{
+			_completed.push({ completeType: CompleteTypes.Failed: true, error: error });
+
+			_isChainProcessed = false;
+
+			failedInternal(error);
+
+			if(_isChainProcessed)
+			{
+				processedDequeue();
+			}
+		}
+
+		function failedInternal(error)
+		{
+			// If at least one is true and no one is false - continue execution.
+			var errorFlag = callError(error),
+				errorComonFlag = _this.errorCommon(error),
+				isContinue = (errorFlag === true || errorComonFlag === true) 
+					&& (errorFlat !== false && errorCommonFlag !== false);
+
+			if(isContinue)
+			{
+				continueExecution(error);
+			}
+			else
+			{
+				_isChainProcessed = true;
+			}
+		}
+
+		// The method that should be called to execute sync success callback.
+		function callSuccess(callResult)
+		{
+			if(_success instanceof Function)
+			{
+				return callSafe(this, _success, [callResult]);
+			}
+		}
+
+		// The method that should be called to execute sync success common callback.
+		this.successCommon(callResult)
+		{
+			var 	successCommonResult = callSuccessCommon(callResult),
+				deferredSuccessCommonResult = callDeferredSuccessCommon(callResult),
+				result;
+
+			if(typeof(successCommonResult) === TypesNames.Boolean)
+			{
+				result = successCommonResult;
+			}
+
+			if(typeof(deferredSuccessCommonResult) === TypesNames.Boolean)
+			{
+				result = result & deferredSuccessCommonResult;
+			}
+
+			return result;
+		}
+
+		// The method that should be called to execute current sync success common callback.
+		function callSuccessCommon(callResult)
+		{
+			var result;
+
+			if(_successCommon instanceof Function)
+			{
+				result = callSafe(this, _successCommon, [callResult]);
+			}
+
+			return result;
+		}
+
+		// The method that should be called to execute next deffered in chain sync successs common callback wrapper.
+		function callDeferredSuccessCommon(callResult)
+		{
+			var result;
+
+			if(_deferred !== null)
+			{
+				result = callSafe(this, _deferred.successCommon, [callResult]);
+			}
+
+			return result;
+		}
+
+		// The method that should be called to execute sync error callback.
+		function callError(error)
+		{
+			var result;
+
+			if(_error instanceof Function)
+			{
+				result = callSafe(this, _error, [error]);
+			}
+
+			return result;
+		}
+
+		// The method that calls both current error common and deferred error common handlers and returns true if at least one of them return Boolean true.
+		this.errorCommon = function(error)
+		{
+			// If required to detect does anythin was called in a common chain, we can return hash with a: first - what callback returned, second does callback was called at all.
+			// And then we can & second to determine does was called some common handler or not.
+			var errorCommonResult = callErrorCommon(error),
+				deferredErrorComonResult = callDeferredErrorCommon(error),
+				result = errorCommonResult === true || deferredErrorCommonResult;
+
+			return result;
+		}
+
+		// The method that should be called to execute sync error common callback.
+		function callErrorCommon(error)
+		{
+			var result;
+
+			if(_errorCommon instanceof Function)
+			{
+				result = callSafe(this, _errorCommon, [error]);
+			}
+
+			return result;
+		}
+
+		function callDeferredErrorCommon(error)
+		{
+			var result;
+
+			if(_deferred !== null
+				&& (_deferred.errorCommon instanceof Function))
+			{
+				result = callSafe(this, _deferred.errorCommon, [error]);
+			}
+
+			return result;
+		}
+
+		// The methd that should be called after all current task handlers (success, error, success common, error common) was executed.
+		// This need to continue execution of a current deffered task(s).
+		function continueExecution(currentResult)
+		{
+			if(_callParameters !== null)
+			{
+				callNext(currentResult);
+				_isChainProcessed = true;
+			}
+		}
+
+		// The method that calls operation and process result.
+		function callNext(currentResult)
+		{
+			if(!!_callParameters.isDone)
+			{
+				var     callArgs = createCallArgs(_callParameters.argsCollection, currentResult),
+					callResult = callSafe(_callParameters.context, _callParameters.method, callArgs);
+				processResult(callResult);
+			}
+		}
+
+		function createCallArgs(argsCollection, currentResult)
+		{
+			var result = (argsCollection || []).slice(Zero);
+			result.push(currentResult);
+
+			return result;
+		}
+
+		// The method that process operation call result.
+		function processCallResult(callResult)
+		{
+			if(!isDeferred(callResult))
+			{
+				_callParameters.result = callResult;
+				executedSuccessfully();
+			}
+			else
+			{
+				_callParameters.callResultDeferred = callResult;
+				attachToCallResultDeferredSafe();
+			}
+		}
+
+		function isDeferred(callResult)
+		{
+			var result = typeof(callResult) !== TypesNames.Undefined && callResult !== null
+				&& ((callResult.onSuccess instanceof Function) || (callResult.onError instanceof Function));
+
+			return result;
+		}
+
+		function executedSuccessfully()
+		{
+			_callParameters.isDone = true;
+			callDeferredDone();
+		}
+
+		function callDeferredDone()
+		{
+			if(_deferred !== null &&
+				(_deferred.done instanceof Function))
+			{
+				callSafe(this, _deferred.done, [_callParameters.callResult]);
+			}
+		}
+
+		function callDeferredFailed()
+		{
+			if(deferred !== null
+				&& (_deferred.failed instanceof Function))
+			{
+				callSafe(this, _deferred.failed, [_callParameters.error]);
+			}
+		}
+
+		function attachToCallResultDeferredSafe()
+		{
+			attachToCallResultDeferredOnSucces();
+			attachToCallResultDeferredOnError();
+		}
+
+		function attachCallResultDeferredOnSuccess()
+		{
+			var callResultDferred = _callParameters.callResultDeferred;
+
+			if (callResultDeferred.onSuccess instanceof Function)
+			{
+				callResultDeferred.onSuccess(callResultDeferredSuccess);
+			}
+		}
+
+		function attachCallResultDeferredOnError()
+		{
+			var callResultDferred = _callParameters.callResultDeferred;
+
+			if(callResultDeferred.onError instanceof Function)
+			{
+				callResultDeferred.onError(callResultDeferredError);
+			}
+		}
+
+		// Called when operation is async and returns deffered and it successfully done.
+		function callResultDeferredSuccess(callResult)
+		{
+			_callParameters.callResult = callResult;
+			callDeferredDone();
+		}
+
+		// Called when operation is async and returns deferred and it done with an error.
+		function callResultDeferredError(error)
+		{
+			_callParameters.error = error;
+			callDeferredFailed();
+		}
+
+		function callSafe(context, method, argsCollection)
+		{
+			try
+			{
+				return method.apply(context, argsCollection);
+			}
+			catch(error)
+			{
+				logError(error);
+			}
+		}
+
+		// The method that specifies a callback function that will be called synchronously when a current task completed.
+		// To continue asyn chain use async methods paradigm (single, all, any, ...)
+		this.onSuccess = function(callback)
+		{
+			_success = callback;
+		}
+
+		// The method that specifies a callback method that will be called synchronously when a current task completed.
+		this.onError = function(callback)
+		{
+			_error = callback;
+		}
+
+		// The method that specifies common sync success handler.
+		this.onSuccessCommon = function(callback)
+		{
+			_successCommon = callback;
+		}
+
+		// The method that specifies common sync error handler.
+		this.onErrorCommon = function(callback)
+		{
+			_errorCommon = callback;
+		}
+
+		// Private
+		// Queue
+		function enqueue(argsCollection)
+		{
+			_callParameters =
+				{
+					context = argsCollection[Zero];
+					method = argsCollection[One];
+					argsColection = Array.prototype.slice.call(argsCollection, Two) || [];
+				};
+
+		}
+		// Deferred
+		function createDeferred()
+		{
+			_deferred = new constructor();
+		}
+
+		// Other
+		function completedDequeue()
+		{
+			_completed.splice(Zero, One);
+		}
+
+		// Helpers
+		function arrayCelear(array)
+		{
+			while(array.length !== Zero)
+			{
+				array.pop();
+			}
+		}
+
+		function logError(error)
+		{
+			if(logger != null)
+			{
+				logger.logError(error);
+			}
+		}
+
+	}
+
+	constructor.single = function(contex, func)
+	{
+		return func.apply(context || this, func, Array.prototype.slice.call(arguments, Two));
+	}
+
+	//return constructor;
+
+	if(typeof mountPoint !== TypesNames.Undefined)
+	{
+		mountPoint.Deferred = constructor;
+	}
+})();
